caa0e6bd6d4e38d85e2069b954243684f5a2cbcc
ayushtues <ayushtues@gmail.com>
2026-01-16 10:06:12 +0530

    HW1 solns

--- hw1/cs285/infrastructure/utils.py
+++ hw1/cs285/infrastructure/utils.py
@@ -9,6 +9,7 @@ from collections import OrderedDict
 import cv2
 import numpy as np
 import time
+import torch
 
 from cs285.infrastructure import pytorch_util as ptu
 
@@ -31,17 +32,17 @@ def sample_trajectory(env, policy, max_path_length, render=False):
             else:
                 img = env.render(mode='single_rgb_array')
             image_obs.append(cv2.resize(img, dsize=(250, 250), interpolation=cv2.INTER_CUBIC))
-    
         # TODO use the most recent ob to decide what to do
-        ac = TODO # HINT: this is a numpy array
+        ac = policy(torch.tensor(ob, device=ptu.device, dtype=torch.float).unsqueeze(0)) # HINT: this is a numpy array
         ac = ac[0]
+        ac = ptu.to_numpy(ac)
 
         # TODO: take that action and get reward and next ob
-        next_ob, rew, done, _ = TODO
+        next_ob, rew, done, _ = env.step(ac)
         
         # TODO rollout can end due to done, or due to max_path_length
         steps += 1
-        rollout_done = TODO # HINT: this is either 0 or 1
+        rollout_done = done or (steps == max_path_length) # HINT: this is either 0 or 1
         
         # record result of taking that action
         obs.append(ob)
@@ -85,7 +86,9 @@ def sample_n_trajectories(env, policy, ntraj, max_path_length, render=False):
     """Collect ntraj rollouts."""
 
     paths = []
-    for i in range(ntraj):
+    from tqdm import tqdm
+    print(f'sampling {ntraj} trajectories')
+    for i in tqdm(range(ntraj)):
         # collect rollout
         path = sample_trajectory(env, policy, max_path_length, render)
         paths.append(path)
--- hw1/cs285/scripts/run_hw1.py
+++ hw1/cs285/scripts/run_hw1.py
@@ -132,7 +132,7 @@ def run_training_loop(params):
             # TODO: collect `params['batch_size']` transitions
             # HINT: use utils.sample_trajectories
             # TODO: implement missing parts of utils.sample_trajectory
-            paths, envsteps_this_batch = TODO
+            paths, envsteps_this_batch = utils.sample_trajectories(env, actor, params['batch_size'], params['ep_len'])
 
             # relabel the collected obs with actions from a provided expert policy
             if params['do_dagger']:
@@ -141,7 +141,15 @@ def run_training_loop(params):
                 # TODO: relabel collected obsevations (from our policy) with labels from expert policy
                 # HINT: query the policy (using the get_action function) with paths[i]["observation"]
                 # and replace paths[i]["action"] with these expert labels
-                paths = TODO
+                new_paths = []
+                for path in paths:
+                    inputs = torch.tensor(path['observation'], device=ptu.device, dtype=torch.float)
+                    action_new = expert_policy(inputs)
+                    action_new = ptu.to_numpy(action_new)
+                    new_path = path
+                    new_path['action'] = action_new
+                    new_paths.append(new_path)
+                path = new_paths
 
         total_envsteps += envsteps_this_batch
         # add collected data to replay buffer
@@ -157,12 +165,13 @@ def run_training_loop(params):
           # HINT2: use np.random.permutation to sample random indices
           # HINT3: return corresponding data points from each array (i.e., not different indices from each array)
           # for imitation learning, we only need observations and actions.  
-          ob_batch, ac_batch = TODO
+          batch_size = params['train_batch_size']
+          idxs = np.random.permutation(len(replay_buffer))[:batch_size]
+          ob_batch, ac_batch = replay_buffer.obs[idxs], replay_buffer.acs[idxs]
 
           # use the sampled data to train an agent
           train_log = actor.update(ob_batch, ac_batch)
-          training_logs.append(train_log)
-
+          training_logs.append(train_log) 
         # log/save
         print('\nBeginning logging procedure...')
         if log_video:
@@ -217,11 +226,11 @@ def main():
     parser.add_argument('--ep_len', type=int)
 
     parser.add_argument('--num_agent_train_steps_per_iter', type=int, default=1000)  # number of gradient steps for training policy (per iter in n_iter)
-    parser.add_argument('--n_iter', '-n', type=int, default=1)
+    parser.add_argument('--n_iter', '-n', type=int, default=10)
 
     parser.add_argument('--batch_size', type=int, default=1000)  # training data collected (in the env) during each iteration
     parser.add_argument('--eval_batch_size', type=int,
-                        default=1000)  # eval data collected (in the env) for logging metrics
+                        default=5000)  # eval data collected (in the env) for logging metrics
     parser.add_argument('--train_batch_size', type=int,
                         default=100)  # number of sampled data points to be used per gradient/train step
 
